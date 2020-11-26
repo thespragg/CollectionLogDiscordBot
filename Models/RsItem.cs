58396bb46972d268f3b3325e702e99ebd836cd1a
Thespragg <alistair@aspragg.me>
2020-11-26 18:39:30 +0000

    Added object for deserialising ge prices

---
+++ Models/RsItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CollectionLogBot.Models
+{
+    public class RsItem
+    {
+        public SubItem item { get; set; }
+    }
+
+    public class SubItem
+    {
+        public Price current { get; set; }
+    }
+
+    public class Price
+    {
+        public string price { get; set; }
+    }
+}
