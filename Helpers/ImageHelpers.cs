359bc0ee8a763cd55dd067f76fb3a9a838cf66c8
Thespragg <alistair@aspragg.me>
2020-11-26 18:40:17 +0000

    Added util method for checking if image is cached or not

---
+++ Helpers/ImageHelpers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Png;
+
+namespace CollectionLogBot.Helpers
+{
+    public static class ImageHelpers
+    {
+        private static readonly HttpClient ImageSource = new HttpClient { BaseAddress = new Uri("https://chisel.weirdgloop.org/static/img/osrs-sprite/") };
+
+        public static Image GetImage(string imageName, string baseDir)
+        {
+            var imgPath = Path.Combine(baseDir, "ItemCache", imageName);
+            var fileInCache = File.Exists(imgPath);
+
+            if (fileInCache) return Image.Load(imgPath);
+
+            using var bytes = ImageSource.GetStreamAsync(imageName).Result;
+            var itemImage = Image.Load(bytes, new PngDecoder());
+            itemImage.Save(imgPath);
+            return itemImage;
+        }
+    }
+}
